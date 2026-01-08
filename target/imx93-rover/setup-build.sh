3eb28fd191cf655b6c5e4096d37d8e86276c4d3d
Jeremy Goddard <jagoddard@gmail.com>
2026-01-07 21:10:15 -0800

    Updae setup-build.sh

--- target/imx93-rover/setup-build.sh
+++ target/imx93-rover/setup-build.sh
@@ -10,7 +10,7 @@ export YOCTO_DIR=${BUILD_DIR}/${BUILD_CONFIG}/conf
 export YOCTO_DROPIN_DIR=${PWD}/yocto-dropin
 
 # Create build directory if it doesn't exist
-mkdir -p ${BUILD_DIR}
+mkdir -p ${BUILD_DIR} 
 pushd ${BUILD_DIR}
 
 # Initialize and sync 
@@ -22,14 +22,16 @@ fi
 
 # Setup the build environment
 if [ ! -f ${ENV_SETUP_FILE} ]; then
-MACHINE=imx93-var-som DISTRO=fsl-imx-wayland . var-setup-release.sh ${CONFIG_DIR}
+mkdir -p ${BUILD_CONFIG}
+MACHINE=imx93-var-som DISTRO=fsl-imx-wayland . var-setup-release.sh ${BUILD_CONFIG}
 touch ${ENV_SETUP_FILE}
 fi
 
 # Copy the yocto drop-in configuration files
 cp -r ${YOCTO_DROPIN_DIR}/* ${YOCTO_DIR}/
 
-# Build the image
-./var-start-container.sh
+# Build the image inside Docker container
+./sources/var-host-docker-containers/run.sh  -u 22.04 -w "$(pwd)" -p \
+   --command "source setup-environment build-rover && bitbake fsl-image-gui"
 
 popd
